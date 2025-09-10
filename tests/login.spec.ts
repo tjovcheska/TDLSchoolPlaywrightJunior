39b5990129a1da155ca0914b69d26b3e8466c551
Teodora Jovcheska <teodora.jovcheska@testdevlab.com>
2025-09-10 09:47:42 +0200

    Forth session - Hooks

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,63 +1,78 @@
-import { test, expect, devices } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test.describe('Login functionality', () => {
 
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
   test.describe('Positive test cases', () => {
-    test('should login with valid credentials as standard_user', async ({ page }) => {
-      await page.goto('/');
-      await expect(page.locator('[data-test="username"]')).toBeVisible();
-      await page.fill('[data-test="username"]', 'standard_user');
 
-      await expect(page.locator('[data-test="password"]')).toBeVisible();
-      await page.fill('[data-test="password"]', 'secret_sauce');
-
-      await expect(page.locator('[data-test="login-button"]')).toBeVisible();
-      await page.click('[data-test="login-button"]');
-
-      await expect(page).toHaveURL(/.*inventory\.html/);
+    test.afterEach(async ({ page }) => {
+      await page.getByText('Open Menu').click();
+      await expect(page.locator('[data-test="logout-sidebar-link"]')).toBeVisible();
+      await page.locator('[data-test="logout-sidebar-link"]').click();
     });
 
-    test('should login with valid credentials as standard_user on mobile', async ({ page, browser }) => {
-      // Launch a new browser context with iPhone 6 viewport
-      const context = await browser.newContext({
-        ...devices['iPhone 6'],
-      });
-      const mobilePage = await context.newPage();
+    test('should login with valid credentials as standard_user', async ({ page }) => {
+      await page.locator('[data-test="username"]').fill('standard_user');
+      await page.locator('[data-test="password"]').fill('secret_sauce');
+      await page.locator('[data-test="login-button"]').click();
 
-      await mobilePage.goto('/');
-      await expect(mobilePage.locator('[data-test="username"]')).toBeVisible();
-      await mobilePage.fill('[data-test="username"]', 'standard_user');
+      await expect(page).toHaveURL(/.*inventory.html/);
+    });
 
-      await expect(mobilePage.locator('[data-test="password"]')).toBeVisible();
-      await mobilePage.fill('[data-test="password"]', 'secret_sauce');
+    test('should login with valid credentials as problem_user', async ({ page }) => {
+      await page.locator('[data-test="username"]').fill('problem_user');
+      await page.locator('[data-test="password"]').fill('secret_sauce');
+      await page.locator('[data-test="login-button"]').click();
 
-      await expect(mobilePage.locator('[data-test="login-button"]')).toBeVisible();
-      await mobilePage.click('[data-test="login-button"]');
+      await expect(page).toHaveURL(/.*inventory.html/);
+    });
 
-      await expect(mobilePage).toHaveURL(/.*inventory\.html/);
+    test('should login with valid credentials as visual_user', async ({ page }) => {
+      await page.locator('[data-test="username"]').fill('visual_user');
+      await page.locator('[data-test="password"]').fill('secret_sauce');
+      await page.locator('[data-test="login-button"]').click();
 
-      await context.close();
+      await expect(page).toHaveURL(/.*inventory.html/);
+    });
+
+    test.skip('should logout after login', async ({ page }) => {
+      await page.locator('[data-test="username"]').fill('standard_user');
+      await page.locator('[data-test="password"]').fill('secret_sauce');
+      await page.locator('[data-test="login-button"]').click();
+
+      await expect(page).toHaveURL(/.*inventory.html/);
+
+      await page.getByText('Open Menu').click();
+      await page.locator('[data-test="logout-sidebar-link"]').click();
     });
   });
 
   test.describe('Negative test cases', () => {
+
     test('should login with invalid credentials', async ({ page }) => {
-      await page.goto('/');
-      await expect(page.locator('[data-test="username"]')).toBeVisible();
-      await page.fill('[data-test="username"]', 'Teodora');
+      await page.locator('[data-test="username"]').fill('Teodora');
+      await page.locator('[data-test="password"]').fill('1234');
+      await page.locator('[data-test="login-button"]').click();
 
-      await expect(page.locator('[data-test="password"]')).toBeVisible();
-      await page.fill('[data-test="password"]', '1234');
-
-      await expect(page.locator('[data-test="login-button"]')).toBeVisible();
-      await page.click('[data-test="login-button"]');
-
-      const errorMessage = page.locator('[data-test="error"]');
-      await expect(errorMessage).toBeVisible();
-      await expect(errorMessage).toHaveText(
+      await expect(page.locator('[data-test="error"]')).toBeVisible();
+      await expect(page.locator('[data-test="error"]')).toHaveText(
         'Epic sadface: Username and password do not match any user in this service'
       );
+      await expect(page).toHaveURL('https://www.saucedemo.com/');
+    });
 
+    test('should prevent login with locked out user', async ({ page }) => {
+      await page.locator('[data-test="username"]').fill('locked_out_user');
+      await page.locator('[data-test="password"]').fill('secret_sauce');
+      await page.locator('[data-test="login-button"]').click();
+
+      await expect(page.locator('[data-test="error"]')).toBeVisible();
+      await expect(page.locator('[data-test="error"]')).toHaveText(
+        'Epic sadface: Sorry, this user has been locked out.'
+      );
       await expect(page).toHaveURL('https://www.saucedemo.com/');
     });
   });
